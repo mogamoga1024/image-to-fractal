a2d97cf833c4238808cc5e6cb5f6760295c21d3c
mogamoga <mogamoga.g1thu6@gmail.com>
2024-10-01 16:34:20 +0900

    四分割

--- main.js
+++ main.js
@@ -40,6 +40,68 @@ function imageToFractal(imageData) {
         }
     }
 
-    
+    const blockList = quarterSplit({
+        startX: 0, startY: 0,
+        width: imageData.width,
+        height: imageData.height
+    });
+
+    for (const block of blockList) {
+        drawAverage(imageData, block);
+    }
+}
+
+function quarterSplit(block) {
+    const {startX, startY, width, height} = block;
+
+    if (width < 2 || height < 2) {
+        return [];
+    }
+
+    const halfWidth1 = Math.floor(width / 2);
+    const halfWidth2 = width - halfWidth1;
+    const halfHeight1 = Math.floor(height / 2);
+    const halfHeight2 = height - halfHeight1;
+
+    return [
+        {startX: startX,              startY: startY,               width: halfWidth1, height: halfHeight1},
+        {startX: startX + halfWidth1, startY: startY,               width: halfWidth2, height: halfHeight1},
+        {startX: startX,              startY: startY + halfHeight1, width: halfWidth1, height: halfHeight2},
+        {startX: startX + halfWidth1, startY: startY + halfHeight1, width: halfWidth2, height: halfHeight2},
+    ];
+}
+
+function drawAverage(imageData, block) {
+    const data = imageData.data;
+    const imageWidth = imageData.width;
+    const startX = block.startX;
+    const endX = block.startX + block.width;
+    const startY = block.startY;
+    const endY = block.startY + block.height;
+    const pixelCount = block.width * block.height;
+
+    let averageR = 0;
+    let averageG = 0;
+    let averageB = 0;
+    for (let x = startX; x < endX; x++) {
+        for (let y = startY; y < endY; y++) {
+            const i = x * 4 + (imageWidth * 4) * y;
+            averageR += data[i + 0];
+            averageG += data[i + 1];
+            averageB += data[i + 2];
+        }
+    }
+    averageR = averageR / pixelCount;
+    averageG = averageG / pixelCount;
+    averageB = averageB / pixelCount;
+
+    for (let x = startX; x < endX; x++) {
+        for (let y = startY; y < endY; y++) {
+            const i = x * 4 + (imageWidth * 4) * y;
+            data[i + 0] = averageR;
+            data[i + 1] = averageG;
+            data[i + 2] = averageB;
+        }
+    }
 }
 

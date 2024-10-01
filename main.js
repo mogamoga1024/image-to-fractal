e1728f1aa76961ef6acdfe00fe8a4682973a6c8d
mogamoga <mogamoga.g1thu6@gmail.com>
2024-10-01 15:42:23 +0900

    透過画像対応

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const resultContext = resultCanvas.getContext("2d");
 
 const image = new Image();
 image.src = "image/野獣先輩.png";
+// image.src = "image/0000000000000000000000000000000000000000000000000000000000000000000.png";
 image.onload = () => {
     main();
 };
@@ -23,6 +24,22 @@ function main() {
 }
 
 function imageToFractal(imageData) {
-    // todo
+    const data = imageData.data;
+
+    // 透明な黒は白に
+    for (let i = 0; i < data.length; i += 4) {
+        const r = data[i + 0];
+        const g = data[i + 1];
+        const b = data[i + 2];
+        const a = data[i + 3];
+        if (r + g + b + a === 0) {
+            data[i + 0] = 255;
+            data[i + 1] = 255;
+            data[i + 2] = 255;
+            data[i + 3] = 255;
+        }
+    }
+
+    
 }
 

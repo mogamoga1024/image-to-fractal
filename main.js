ace6630cf5ed705dd53232a0e5a35ec27b88ce23
mogamoga <mogamoga.g1thu6@gmail.com>
2024-10-02 11:25:18 +0900

    れなさん

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 
+const inputFileDom = document.querySelector("#input-file");
 const resultCanvas = document.querySelector("#result");
 const resultContext = resultCanvas.getContext("2d");
 
-const image = new Image();
-// image.src = "image/野獣先輩.png";
-// image.src = "image/0000000000000000000000000000000000000000000000000000000000000000000.png";
-image.src = "image/test.png";
+inputFileDom.onchange = e => {
+    const imageFile = e.target.files[0];
+    
+    if (!imageFile.type.startsWith("image")) {
+        alert("画像ファイルを選択してください。");
+        return;
+    }
 
-image.onload = () => {
+    const image = new Image();
+    image.onload = () => {
+        drawFractal(image);
+        URL.revokeObjectURL(image.src);
+    };
+    image.onerror = () => {
+        alert("画像の読み込みに失敗しました。");
+        URL.revokeObjectURL(image.src);
+    };
+    image.src = URL.createObjectURL(imageFile);
+};
+
+{
+    const image = new Image();
+    // image.src = "image/野獣先輩.png";
+    // image.src = "image/0000000000000000000000000000000000000000000000000000000000000000000.png";
+    // image.src = "image/test.png";
+    image.src = "Lenna.png";
+    image.onload = () => {
+        drawFractal(image);
+    };
+}
+
+function drawFractal(image) {
     let count = 300;
     // let isStroke = true;
     let isStroke = false;
@@ -20,7 +47,5 @@ image.onload = () => {
     resultCanvas.width = result.width;
     resultCanvas.height = result.height;
     resultContext.drawImage(result, 0, 0);
-};
-
-
+}
 
